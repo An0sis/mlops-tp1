94b81d39d8b95afa7b827be1fc0311aa50d543a9
Joric Hantzberg <joric.hantzberg@epita.fr>
2025-09-11 14:55:44 +0200

    feat: level 4 T5 model for summary

--- app/webserver.py
+++ app/webserver.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import joblib
 from pydantic import BaseModel
+from transformers import pipeline
 
 model = joblib.load("app/regression.joblib")
+pipe = pipeline("summarization", model="google-t5/t5-small")
 
 app = FastAPI()
 
@@ -11,11 +13,19 @@ class HouseFeatures(BaseModel):
     size: float
     nb_rooms: int
     garden: bool
+    
+class SummarizationRequest(BaseModel):
+    text: str
 
 @app.post("/predict")
 async def predict(features: HouseFeatures):
     prediction = model.predict([[features.size, features.nb_rooms, features.garden]])[0]
     return {"prediction": prediction}
+
+@app.post("/summarize")
+async def summarize_text(request: SummarizationRequest):
+    summary = pipe(request.text)
+    return {"summary": summary[0]['summary_text']}
 
 @app.get("/")
 async def root():
